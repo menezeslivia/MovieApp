4e4ed303f0a740fc8c391f6119818b2700edfa96
Fernanda Gabrielli <fernandagabriellidev@gmail.com>
2025-12-01 22:41:09 -0300

    resolve: overlay color

--- src/screens/CinemasScreen.js
+++ src/screens/CinemasScreen.js
@@ -447,7 +447,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.13)',
+    backgroundColor: 'rgba(0, 0, 0, 0.62)',
     justifyContent: 'center',
     alignItems: 'center',
   },
